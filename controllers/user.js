d20238e8c2576e17f30bcaabfbb506d6af174c70
aashna956 <makkar2@illinois.edu>
2015-10-11 01:19:02 -0700

    Added username in signup

--- controllers/user.js
+++ controllers/user.js
@@ -85,7 +85,8 @@ exports.postSignup = function(req, res, next) {
 
   var user = new User({
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    username: req.body.username
   });
 
   User.findOne({ email: req.body.email }, function(err, existingUser) {
